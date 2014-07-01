474ba4dd55854bfd172f2ffc467daf6356717d06
Bob <spamcan@p339.com>
2014-06-30 20:44:46 -0500

    Added switch statement to address files needing more information.  Added error reporting to stdout.

--- RenameNeat.go
+++ RenameNeat.go
@@ -1,3 +1,16 @@
+// ** The next iteration of this program should move the resulting file into the file structure that already exists.
+//
+// Written by:  Bob Failla
+//
+// Reads files in a directory provided by the user
+// Parses the files for Name, Amount and Date
+// Renames the files according to YYYYMMDD ($amount) Name
+// Places the files in a directory supplied by the user
+// Leaves the files that could not be renamed in the source directory
+// Prints a message if a file could not be moved
+// Prints a summary of how many files were moved
+
+
 package main
 
 import (
@@ -7,8 +20,9 @@ import (
 	"os"
 )
 var amount_re = regexp.MustCompile(`\d+\.\d+`)
-var date_re = regexp.MustCompile(`\s\d+\s`)
-var name_re = regexp.MustCompile(`[A-Za-z _]+`)
+var date_re = regexp.MustCompile(`\d{8}`)
+// \d}[\s]\d+\s`)
+var name_re = regexp.MustCompile(`[A-Za-z\s_]+`)
 var dr_list []os.FileInfo
 var destination, source, amount, date, name, s, d, sf, df string
 var v os.FileInfo
@@ -46,7 +60,7 @@ func move_the_file (source, sf, destintion, df string) (n int) {
 			fmt.Printf("Unable to create the directory \n")
 			os.Exit(1)
 	}
-	os.Rename(source+sf, destination+df)
+	os.Rename(source+sf, destination+df+ ".pdf")
 	if err == nil {
 		n = 1
 	} else {
@@ -62,7 +76,7 @@ func main() {
 	get_input()
 // Validate source and destination input.  Exit with error code 1 if not valid.
 	if !input_is_valid(source) {
-		fmt.Printf("Source direcgory is invalid.  A-Z, a-z, 0-9, /-_ only")
+		fmt.Printf("Source directory is invalid.  A-Z, a-z, 0-9, /-_ only")
 		os.Exit(1)
 	}
 	if !input_is_valid(destination) {
@@ -78,55 +92,64 @@ func main() {
 //iterate over the file names, call the parsing function and if possible rewrite the file names
 	for _, v = range (dr_list) {
 		amount, date, name = get_file_data(v)
-		switch {
+		sf := "/" + v.Name()
+		switch  {
 		case (amount != "") && (date != "") && (name != ""): {
-			sf := "/" + v.Name()
-			df := "/" + date + "($" + amount + ") " + name + ".pdf"
+			// missing nothing - **********************************								OK
+			df := "/" + date + " ($" + amount + ") " + name
 			fm = fm+move_the_file(source, sf, destination, df)
 		}
 		case (amount != "") && (date != "") && (name == ""): {
 			// missing name only
+			df := "/" + date + " ($" + amount + ") " + "NEED_NAME.pdf"
+			fm = fm+move_the_file(source, sf, destination, df)
 		}
 		case (amount != "") && (date == "") && (name != ""): {
 			// missing date only
+			df := "/" + " NEED DATE " + " ($" + amount + ") " + name
+			fm = fm+move_the_file(source, sf, destination, df)
 		}
 		case (amount != "") && (date == "") && (name == ""): {
-			// missing date and name
-		}
-		case (amount == "") && (date != "") && (name != ""): {
-			// missing everything
-		}
-		case (amount == "") && (date != "") && (name == ""): {
-			//missing amount and name
-		}
-		case (amount == "") && (date == "") && (name != ""): {
-			// missing amount and date
+			// missing date and name *****************************    							Not OK
+			df := "/" + " NEED DATE" + " ($" + amount + ") " + "NEED NAME.pdf"
+			fm = fm+move_the_file(source, sf, destination, df)
 		}
 		case (amount == "") && (date != "") && (name != ""): {
 			// missing amount only
+			df := "/" + date + " ($" + "NEED AMOUNT" + ") " + name
+			fm = fm+move_the_file(source, sf, destination, df)
 		}
-
-
-			// A	D	N
-			// 0	0	0 - Everything missing 				- done
-			// 0	0	1 - Missing amount and date			- done
-			// 0	1	0 = Missing  amount and name		- done
-			// 0	1	1 = Missing amount only				- done
-			// 1	0	0 = Missing date and name			- done
-			// 1	0	1 = Missing date only				- done
-			// 1	1	0 = Missing name only				- done
-			// 1	1	1 = Nothing missing					- done
-
-
-
-			//		if (amount != "") && (date != "") && (name != "") {
-			//			sf := "/" + v.Name()
-			//			df := "/" + date + "($" + amount + ") " + name + ".pdf"
-			//			fm = fm + move_the_file(source, sf, destination, df)
-			//		}
+		default : {
+			fmt.Printf("Not enough data to move file: %s\n", sf)
+			// missing everything
+			// missing amount and name
+			// missing amount and date
+			// do nothing
+		}
 		}
 	}
-	fmt.Printf("%d" +  " files renamed and moved", fm)
+	fmt.Printf("\n\n%d" +  " files renamed and moved\n", fm)
 }
+
+
+
+// A	D	N
+// 0	0	0 - Everything missing 				- done
+// 0	0	1 - Missing amount and date			- done
+// 0	1	0 = Missing  amount and name		- done
+// 0	1	1 = Missing amount only				- done
+// 1	0	0 = Missing date and name			- done
+// 1	0	1 = Missing date only				- done
+// 1	1	0 = Missing name only				- done
+// 1	1	1 = Nothing missing					- done
+
+
+
+//		if (amount != "") && (date != "") && (name != "") {
+//			sf := "/" + v.Name()
+//			df := "/" + date + "($" + amount + ") " + name + ".pdf"
+//			fm = fm + move_the_file(source, sf, destination, df)
+//		}
+
 
 
